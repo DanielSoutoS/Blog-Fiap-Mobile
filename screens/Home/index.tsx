5448b6450a668c047ba20567201812346f3f8e55
Daniel Saraiva <danielsoutosaraiva@gmail.com>
2024-11-23 16:26:20 -0300

    Rodado o comando - npm run reset-project

---
+++ screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { useState } from "react";
+
+
+export function Home() {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+}
